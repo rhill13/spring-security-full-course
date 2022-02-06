6bac129ec882edab3e27ee8eed21eac765b184ea
Ryan Hill <ryanhill1128@gmail.com>
2022-02-06 09:17:30 -0600

    simple api with basic form based authentication

---
+++ spring-security-full/src/main/java/io/rookware/springsecurityfull/student/Student.java
@@ -0,0 +1,13 @@
+package io.rookware.springsecurityfull.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor
+public class Student {
+
+    private Integer id;
+    private String name;
+
+}
