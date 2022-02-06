dc78a12684d83806cb7efe7a38371caba4aa3a28
Ryan Hill <ryanhill1128@gmail.com>
2022-02-06 11:02:37 -0600

    adding users using 'in memory user details'; encode with bcrypt

--- spring-security-full/src/main/java/io/rookware/springsecurityfull/security/ApplicationSecurityConfig.java
+++ spring-security-full/src/main/java/io/rookware/springsecurityfull/security/ApplicationSecurityConfig.java
@@ -1,13 +1,24 @@
 package io.rookware.springsecurityfull.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -17,5 +28,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser
+        );
     }
 }
