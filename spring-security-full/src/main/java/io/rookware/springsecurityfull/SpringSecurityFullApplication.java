6b041c67b8eed0327bb600af0917a70e701d0346
Ryan Hill <ryanhill1128@gmail.com>
2022-02-06 08:58:08 -0600

    initial commit

---
+++ spring-security-full/src/main/java/io/rookware/springsecurityfull/SpringSecurityFullApplication.java
@@ -0,0 +1,13 @@
+package io.rookware.springsecurityfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFullApplication.class, args);
+	}
+
+}
---
+++ spring-security-full/src/test/java/io/rookware/springsecurityfull/SpringSecurityFullApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rookware.springsecurityfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
